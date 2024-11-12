3f80e18e3966b72b9aa9beca4c6638f043affdb1
Francisco Douglas <franciscodougllas@outlook.com>
2024-11-12 11:00:34 -0300

    [feat] aula 11 - introdução ao php

---
+++ Aula 11 - PHP I/var.php
@@ -0,0 +1,24 @@
+<?php
+    $nome = 'Francisco';
+    $funcao = "Professor";
+    echo "Nome: $nome <br>";
+    echo "Função: $funcao <br>";
+
+    $altura = 1.75;
+    $peso = 80;
+    echo "Altura: $altura <br>";
+    echo "Peso: $peso <br>";
+
+    $situacao = true;
+    echo "Situação: $situacao <br>";
+    
+    $situacao = false;
+    echo "Situação: $situacao <br>";
+    
+    $situacao = null;
+    echo "Situação: $situacao <br>";
+
+    $disciplinas = array("Programação Web", "Banco de Dados","Estrutura de dados", "Contabilidade");
+    print_r($disciplinas);
+
+?>
